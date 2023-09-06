0df549bd8a67260922077b770871c327974e0fd6
philq <philq.developer@gmail.com>
2023-09-06 09:12:53 -0400

    Add files via upload

---
+++ static/feature.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var imageContainer = document.querySelector(".image-container");
+  if (!imageContainer) {
+    console.warn("Image container not found");
+    return;
+  }
+  var images = imageContainer.querySelectorAll("img");
+  var currentImage = 0;
+
+  function switchImage() {
+    // Hide the current image
+    images[currentImage].style.display = "none";
+
+    // Move to the next image
+    currentImage = (currentImage + 1) % images.length;
+
+    // Show the next image
+    images[currentImage].style.display = "block";
+  }
+
+  // Initially show the first image
+  images[currentImage].style.display = "block";
+
+  // Set up an interval to switch the image every 5 seconds
+  setInterval(switchImage, 5000);
+});
